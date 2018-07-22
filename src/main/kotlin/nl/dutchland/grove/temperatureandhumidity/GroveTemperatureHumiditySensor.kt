55362c21eb83c4a25f3d2198233ee987fc22fcde
Unknown <marcelotto89@gmail.com>
2018-07-22 14:43:10 +0200

    Little clean up

--- src/main/kotlin/nl/dutchland/grove/temperatureandhumidity/GroveTemperatureHumiditySensor.kt
+++ src/main/kotlin/nl/dutchland/grove/temperatureandhumidity/GroveTemperatureHumiditySensor.kt
@@ -21,59 +21,50 @@ class GroveTemperatureHumiditySensor internal constructor(private val sensor : G
     override fun subscribeToTemperatureHumidity(listener: TemperatureHumidityListener, pollInterval: Period) {
         val intervalInMilliseconds : Long = pollInterval.valueIn(Millisecond).toLong()
         fixedRateTimer("Calling listener", false, intervalInMilliseconds, intervalInMilliseconds)
-        { callTemperatureHumidityListener(listener) }
+        { listener.invoke(mostRecentValue) }
 
-        callTemperatureHumidityListener(listener)
+        listener.invoke(mostRecentValue)
     }
 
     override fun subscribeToTemperature(listener: TemperatureListener, pollInterval: Period) {
         val intervalInMilliseconds : Long = pollInterval.valueIn(Millisecond).toLong()
         fixedRateTimer("Calling listener", false, intervalInMilliseconds, intervalInMilliseconds)
-        { callTemperatureListener(listener) }
+        { listener.invoke(mostRecentValue.toTemperatureMeasurement()) }
 
-        callTemperatureListener(listener)
+        listener.invoke(mostRecentValue.toTemperatureMeasurement())
     }
 
     override fun subscribeToHumidity(listener: HumidityListener, pollInterval: Period) {
         val intervalInMilliseconds : Long = pollInterval.valueIn(Millisecond).toLong()
         fixedRateTimer("Calling listener", false, intervalInMilliseconds, intervalInMilliseconds)
-        { callHumidityListener(listener) }
+        { listener.invoke(mostRecentValue.toHumidityMeasurement()) }
 
-        callHumidityListener(listener)
+        listener.invoke(mostRecentValue.toHumidityMeasurement())
     }
 
     override fun getTemperatureHumidity(): TemperatureHumidityMeasurement {
         val sensorValue = this.sensor.get()
 
+        val timeStamp = TimeStamp.fromMillisecondsSinceEpoch(System.currentTimeMillis())
         val humidity = RelativeHumidity(FractionalPercentage.ofPercentage(sensorValue.humidity))
         val temperature = Temperature.of(sensorValue.temperature, Celcius)
-        val timeStamp = TimeStamp.fromMillisecondsSinceEpoch(System.currentTimeMillis())
+
         return TemperatureHumidityMeasurement(temperature, humidity, timeStamp)
     }
 
     override fun getHumidity(): HumidityMeasurement {
-        val temperatureAndHumidity = getTemperatureHumidity()
-        return HumidityMeasurement(temperatureAndHumidity.humidity, temperatureAndHumidity.timeStamp)
+        return getTemperatureHumidity().toHumidityMeasurement()
     }
 
     override fun getTemperature(): TemperatureMeasurement {
-        val temperatureAndHumidity = getTemperatureHumidity()
-        return TemperatureMeasurement(temperatureAndHumidity.temperature, temperatureAndHumidity.timeStamp)
+        return getTemperatureHumidity().toTemperatureMeasurement()
     }
 
-    private fun callTemperatureListener(listener: TemperatureListener) {
-        listener.invoke(TemperatureMeasurement(
-                this.mostRecentValue.temperature,
-                this.mostRecentValue.timeStamp))
+    private fun TemperatureHumidityMeasurement.toHumidityMeasurement() : HumidityMeasurement {
+        return HumidityMeasurement(this.humidity, this.timeStamp)
     }
 
-    private fun callHumidityListener(listener: HumidityListener) {
-        listener.invoke(HumidityMeasurement(
-                this.mostRecentValue.humidity,
-                this.mostRecentValue.timeStamp))
-    }
-
-    private fun callTemperatureHumidityListener(listener: TemperatureHumidityListener) {
-        listener.invoke(this.mostRecentValue)
+    private fun TemperatureHumidityMeasurement.toTemperatureMeasurement() : TemperatureMeasurement {
+                return TemperatureMeasurement(this.temperature, this.timeStamp)
     }
 }
