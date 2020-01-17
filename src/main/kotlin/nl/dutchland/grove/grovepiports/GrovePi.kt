226403472fd43efc0b30c5d28506a964fe3291b3
Unknown <marcelotto89@gmail.com>
2020-01-17 19:40:31 +0100

    Added GrovePi for regular Grove shield

---
+++ src/main/kotlin/nl/dutchland/grove/grovepiports/GrovePi.kt
@@ -0,0 +1,61 @@
+package nl.dutchland.grove.grovepiports
+
+object GrovePi {
+    object A0 : DigitalPort, AnalogPort {
+        override val digitalPin = 14
+        override val analogPin = 0
+    }
+
+    object A1 : DigitalPort, AnalogPort {
+        override val digitalPin = 15
+        override val analogPin = 1
+    }
+
+    object A2 : DigitalPort, AnalogPort {
+        override val digitalPin = 16
+        override val analogPin = 2
+    }
+
+    object D2 : DigitalPort {
+        override val digitalPin = 2
+    }
+
+    object D3 : PulseWidthModulationPort {
+        override val digitalPin = 3
+    }
+
+    object D4 : PulseWidthModulationPort {
+        override val digitalPin = 4
+    }
+
+    object D5 : PulseWidthModulationPort {
+        override val digitalPin = 5
+    }
+
+    object D6 : PulseWidthModulationPort {
+        override val digitalPin = 6
+    }
+
+    object D7 : DigitalPort {
+        override val digitalPin = 7
+    }
+
+    object D8 : DigitalPort {
+        override val digitalPin = 8
+    }
+
+    object I2c1 : I2cPort {
+        override val i2cNumber = 1
+    }
+
+    object I2c2 : I2cPort {
+        override val i2cNumber = 2
+    }
+
+    object I2c3 : I2cPort {
+        override val i2cNumber = 3
+    }
+
+    object RPISER
+    object SERIAL
+}
